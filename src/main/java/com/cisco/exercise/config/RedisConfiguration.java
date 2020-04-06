b37cacce6dd3ee8c09282cb165067a36ae968364
Mayur Barge <bargemayur05@gmail.com>
2020-04-05 17:16:20 -0700

    MalwareURL entity along with repository integration test

---
+++ src/main/java/com/cisco/exercise/config/RedisConfiguration.java
@@ -0,0 +1,19 @@
+package com.cisco.exercise.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+
+@Configuration
+@EnableRedisRepositories
+class RedisConfiguration {
+
+    @Bean
+    public JedisConnectionFactory redisConnectionFactory() {
+
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
+        return new JedisConnectionFactory(config);
+    }
+}
