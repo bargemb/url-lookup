6c9b768de50e1a41527149986a3ff08254a57e6e
Mayur Barge <bargemayur05@gmail.com>
2020-04-07 01:04:35 -0700

    Removing dependecy of redis server for unit and integration testing. Used embedded redis.

---
+++ src/test/java/com/cisco/exercise/config/TestRedisConfiguration.java
@@ -0,0 +1,28 @@
+package com.cisco.exercise.config;
+
+
+import org.springframework.boot.test.context.TestConfiguration;
+import redis.embedded.RedisServer;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+@TestConfiguration
+public class TestRedisConfiguration {
+    private RedisServer redisServer;
+
+    public TestRedisConfiguration() {
+        this.redisServer = new RedisServer();
+    }
+
+    @PostConstruct
+    public void postConstruct() {
+
+        redisServer.start();
+    }
+
+    @PreDestroy
+    public void preDestroy() {
+        redisServer.stop();
+    }
+}
